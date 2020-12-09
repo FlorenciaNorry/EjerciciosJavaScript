170f253057c1900fc4492ab8ac08dc5bceeedf76
Florencia norry <maflorencia.norry@gmail.com>
2020-12-09 20:41:09 -0300

    Se realiza el ejercicio 5

---
+++ js/ejercicio5.js
@@ -0,0 +1,3 @@
+let numero1 = parseInt(prompt("Inrese el primer numero"));
+let numero2 = parseInt(prompt("Ingrese el segundo numero"));
+document.write("La suma de ambos numeros es: " + (numero1 + numero2));
