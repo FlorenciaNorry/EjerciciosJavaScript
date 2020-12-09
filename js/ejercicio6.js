5475f7b4584144744fc5154039f185f45dc48d0a
Florencia norry <maflorencia.norry@gmail.com>
2020-12-09 20:48:41 -0300

    se realiza el ejercicio 6

---
+++ js/ejercicio6.js
@@ -0,0 +1,7 @@
+let numero1 = parseInt(prompt("Ingrese el primer numero"));
+let numero2 = parseInt(prompt("Ingrese el seundo numero"));
+if(numero1>numero2){
+    document.write("El numero mayor es " + numero1);
+}else{
+    document.write("El numero mayor es " + numero2);
+}
