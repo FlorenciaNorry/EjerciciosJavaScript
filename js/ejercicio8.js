6753eb752742e37e1f1415627b9c7a1ffde4d919
Florencia norry <maflorencia.norry@gmail.com>
2020-12-09 21:42:32 -0300

    se realiza el ejercicio8

---
+++ js/ejercicio8.js
@@ -0,0 +1,7 @@
+let numero = prompt("Ingrese un numero");
+
+if(numero % 2 == 0){
+    document.write("el numero " + numero + "  es divisible en 2");
+}else{
+    document.write("El numero " + numero + "  no es divisible en 2");
+}
